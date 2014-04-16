49838b78d6fa16a37d56f57bc62e8a23ead80319
Tom Yeh <doubleshow@gmail.com>
2014-04-16 16:40:21 -0400

    add media, but without file

---
+++ import.py
@@ -0,0 +1,68 @@
+from openpyxl.reader.excel import load_workbook
+
+from db_def import db
+from db_def import Account
+from db_def import Note
+from db_def import Media
+from db_def import Context
+
+db.drop_all()
+db.create_all()
+
+
+wb = load_workbook(filename = r'data.xlsx')
+account_sheet = wb.get_sheet_by_name(name = 'Account')
+note_sheet = wb.get_sheet_by_name(name = 'Note')
+context_sheet = wb.get_sheet_by_name(name = 'Context')
+
+n = account_sheet.cell('A1').value
+for i in range(2,2+n):
+	id = account_sheet.cell('A' + str(i)).value	
+	username = account_sheet.cell('B' + str(i)).value
+	account = Account(username)
+	if id:
+		print "create account: %s" % account
+		db.session.add(account)
+		db.session.commit()
+
+n = context_sheet.cell('A1').value
+for i in range(2,2+n):
+	id = note_sheet.cell('A' + str(i)).value	
+	kind = context_sheet.cell('B' + str(i)).value
+	name = context_sheet.cell('C' + str(i)).value
+	description = context_sheet.cell('D' + str(i)).value
+	context = Context(kind, name, description)
+
+	if id:
+		print "create context: %s" % context
+		db.session.add(context)
+		db.session.commit()
+
+
+n = note_sheet.cell('A1').value
+for i in range(2,2+n):
+	id = note_sheet.cell('A' + str(i)).value	
+	username = note_sheet.cell('B' + str(i)).value	
+	context  = note_sheet.cell('C' + str(i)).value
+	kind     = note_sheet.cell('D' + str(i)).value
+	content  = note_sheet.cell('E' + str(i)).value	
+
+	media_kind  = note_sheet.cell('F' + str(i)).value	
+	media_title = note_sheet.cell('G' + str(i)).value	
+	media_url = note_sheet.cell('H' + str(i)).value	
+
+	if id:
+		a = Account.query.filter_by(username=username).first()
+		c = Context.query.filter_by(name=context).first()
+		note = Note(a.id, c.id, kind, content)
+		print "create note: %s" % note
+		db.session.add(note)
+		db.session.commit()
+		
+		media = Media(note.id, media_kind, media_title, media_url) 
+		print "create media: %s" % media
+		db.session.add(media)
+		db.session.commit()
+
+
+#print sheet_ranges.cell('B2').value # D18
---
+++ tests/src/test/java/net/nature/api/test/MediaTest.java
@@ -0,0 +1,41 @@
+package net.nature.api.test;
+
+import static com.eclipsesource.restfuse.Assert.assertOk;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import com.eclipsesource.restfuse.Destination;
+import com.eclipsesource.restfuse.HttpJUnitRunner;
+import com.eclipsesource.restfuse.Method;
+import com.eclipsesource.restfuse.Response;
+import com.eclipsesource.restfuse.annotation.Context;
+import com.eclipsesource.restfuse.annotation.HttpTest;
+import com.jayway.jsonassert.JsonAssert;
+
+@RunWith( HttpJUnitRunner.class )
+public class MediaTest {
+
+	@Rule
+	public Destination destination = new Destination(this, "http://localhost:5000");
+
+	@Context
+	private Response response; // will be injected after every request
+
+	
+	@HttpTest(method = Method.POST, 
+			path = "/api/media/new",
+			content = "{ \"title\" : \"some new media\"" +
+					", \"note_id\" : 2" +
+					", \"kind\" : \"Photo\"}  ")
+	public void  create_succeed() {
+		assertOk(response);
+		String json = response.getBody();		
+		System.out.println(json);
+		JsonAssert.with(json).assertThat("$media.kind", equalTo("Photo"));
+		JsonAssert.with(json).assertThat("$media.title", equalTo("some new media"));
+	}
+	
+	
+}

7ab963634f9f503383331ec1d2e18f11c55000df
Tom Yeh <doubleshow@gmail.com>
2014-05-15 16:22:32 -0600

    debug

--- api.py
+++ api.py
@@ -199,11 +199,11 @@ def api_media_create(id):
 	if request.method == 'POST':
 		print request.files
 		print request.form
-		link = request.files["link"] or ""
-		title = request.files["title"] or ""
+		link = request.files["link"] or request.form["link"] or ""
+		title = request.files["title"] or request.form["title"] or ""
 		kind = "Photo"
 		note = Note.query.get(int(id))
-		print note
+		print "note: " + note
 		if note:
 			media = Media(note.id, kind, title, link) 
 			db.session.add(media)

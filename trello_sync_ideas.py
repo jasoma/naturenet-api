0b7bf430584c8bac36b99bed6f06d5618a608915
Mohammad Javad Mahzoon <MJ@Mohammads-MacBook-Pro.local>
2014-08-13 22:10:34 -0400

    debug

--- trello_sync_ideas.py
+++ trello_sync_ideas.py
@@ -57,44 +57,44 @@ for c in cards:
             # db.session.add(feedback)
             # db.session.commit()
         else:
-            f = Feedback.query.filter(Feedback.table_name.ilike('note'), Feedback.row_id==note.id, Feedback.kind=='comment', Feedback.content==comment['data']['text'])
+            f = Feedback.query.filter(Feedback.table_name.ilike('note'), Feedback.row_id==note.id, Feedback.kind=='comment', Feedback.content==comment['data']['text']).first()
             if not f:
                 feedback = Feedback(0, 'comment', comment['data']['text'], 'note', note.id, 0)
                 db.session.add(feedback)
                 db.session.commit()
 
 # second pass (server -> trello)
-print "SECOND PASS"
-ideas = Note.query.filter(Note.kind.ilike('designidea'), Note.context_id==context.id).all()
-print "%s ideas to sync." % len(ideas)
-for i in ideas:
-    # look if it does not exists in trello create it, else update it
-    card = trello_api.get_card_by_id(i.id)
-    feedbacks_comment = Feedback.query.filter(Feedback.table_name.ilike('note'), Feedback.row_id==i.id, Feedback.kind=='comment').all()
-    feedbacks_like = Feedback.query.filter(Feedback.table_name.ilike('note'), Feedback.row_id==i.id, Feedback.kind=='like').all()
-    new_desc = i.to_trello_desc() + "\r\n#likes: " + str(len(feedbacks_like))# + "\r\n#comments: " + str(len(feedbacks_comment))
-    new_card = None
-    if not card:
-        # create it
-        if not i.status or i.status == '':
-            i.status = trello_api.DEFAULT_LIST
-            i.modified_at = datetime.now()
-        list_name = i.status
-        list_id = trello_api.get_list_id(list_name)
-        if not list_id:
-            i.status = trello_api.DEFAULT_LIST
-            list_name = trello_api.DEFAULT_LIST
-        new_card = trello_api.add_card(i.id, i.content, new_desc, list_name, use_default_list=True)
-    # updating the comments
-    for comment in feedbacks_comment:
-        account = Account.query.filter_by(id=comment.account_id).first()
-        name = 'The Design Team'
-        if account:
-           name = account.username
-        if new_card:
-            trello_api.add_comment_card(i.id, new_card.name, "[" + name + "] " + comment.content)
-        else:
-            e = trello_api.comment_exists(card, comment.content)
-            if not e:
-                trello_api.add_comment_card(i.id, card.name, "[" + name + "] " + comment.content)
-db.session.commit()
+# print "SECOND PASS"
+# ideas = Note.query.filter(Note.kind.ilike('designidea'), Note.context_id==context.id).all()
+# print "%s ideas to sync." % len(ideas)
+# for i in ideas:
+#     # look if it does not exists in trello create it, else update it
+#     card = trello_api.get_card_by_id(i.id)
+#     feedbacks_comment = Feedback.query.filter(Feedback.table_name.ilike('note'), Feedback.row_id==i.id, Feedback.kind=='comment').all()
+#     feedbacks_like = Feedback.query.filter(Feedback.table_name.ilike('note'), Feedback.row_id==i.id, Feedback.kind=='like').all()
+#     new_desc = i.to_trello_desc() + "\r\n#likes: " + str(len(feedbacks_like))# + "\r\n#comments: " + str(len(feedbacks_comment))
+#     new_card = None
+#     if not card:
+#         # create it
+#         if not i.status or i.status == '':
+#             i.status = trello_api.DEFAULT_LIST
+#             i.modified_at = datetime.now()
+#         list_name = i.status
+#         list_id = trello_api.get_list_id(list_name)
+#         if not list_id:
+#             i.status = trello_api.DEFAULT_LIST
+#             list_name = trello_api.DEFAULT_LIST
+#         new_card = trello_api.add_card(i.id, i.content, new_desc, list_name, use_default_list=True)
+#     # updating the comments
+#     for comment in feedbacks_comment:
+#         account = Account.query.filter_by(id=comment.account_id).first()
+#         name = 'The Design Team'
+#         if account:
+#            name = account.username
+#         if new_card:
+#             trello_api.add_comment_card(i.id, new_card.name, "[" + name + "] " + comment.content)
+#         else:
+#             e = trello_api.comment_exists(card, comment.content)
+#             if not e:
+#                 trello_api.add_comment_card(i.id, card.name, "[" + name + "] " + comment.content)
+# db.session.commit()
